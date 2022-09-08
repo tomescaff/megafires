2ffa5e9fd47fad8bcf7004ce2111393a92d61c2e
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-09-08 19:47:04 -0400

    added function for computing trends

--- processing/processing/utils.py
+++ processing/processing/utils.py
@@ -523,3 +523,42 @@ def get_tau(z):
 
     return u, tau
 
+# compute annual trend from [run, time, lat, lon] xarray DataArray
+def compute_trend(xr_array):
+
+    # get shape
+    nrun, ntime, nlat, nlon = xr_array.values.shape
+
+    # initalize matrix with results
+    matrix_slope = np.zeros((nrun,nlat,nlon))
+    matrix_intercept = np.zeros((nrun,nlat,nlon))
+    matrix_rvalue = np.zeros((nrun,nlat,nlon))
+    matrix_pvalue = np.zeros((nrun,nlat,nlon))
+
+    # for each run-lat-lon
+    for ir in range(nrun):
+        for ilat in range(nlat):
+            for ilon in range(nlon):
+
+                # get the series
+                series = xr_array[ir,:,ilat,ilon]
+
+                # get the x and y values
+                y = series.values
+                x = series.time.dt.year.values
+
+                # compute the linear regression of y onto x
+                slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+
+                # save results
+                matrix_slope[ir, ilat, ilon] = slope
+                matrix_intercept[ir, ilat, ilon] = intercept
+                matrix_rvalue[ir, ilat, ilon] = r_value
+                matrix_pvalue[ir, ilat, ilon] = p_value
+
+    da_slope = xr.DataArray(matrix_slope, coords = [xr_array.run, xr_array.lat, xr_array.lon], dims=['run', 'lat', 'lon'])
+    da_intercept = xr.DataArray(matrix_intercept, coords = [xr_array.run, xr_array.lat, xr_array.lon], dims=['run', 'lat', 'lon'])
+    da_rvalue = xr.DataArray(matrix_rvalue, coords = [xr_array.run, xr_array.lat, xr_array.lon], dims=['run', 'lat', 'lon'])
+    da_pvalue = xr.DataArray(matrix_pvalue, coords = [xr_array.run, xr_array.lat, xr_array.lon], dims=['run', 'lat', 'lon'])
+    ds_out = xr.Dataset({'slope':da_slope, 'intercept':da_intercept, 'rvalue':da_rvalue, 'pvalue':da_pvalue})
+    return ds_out
