8a8c84468283e869563f62883edcd89d068e6ca2
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-10-14 10:52:20 -0300

    added nearest neighbor attribution

---
+++ processing/attribution_nearest_neighbor/bootstrap_LENS1_MLE_normfit_1920_2021_evaluation_NN.py
@@ -0,0 +1,42 @@
+import sys
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+from sklearn.utils import resample as bootstrap
+
+sys.path.append('../../processing')
+
+import processing.gmst as gmst
+import processing.lens as lens
+import processing.math as pmath
+
+lens1_gmst_full = gmst.get_gmst_annual_lens1_ensmean()
+lens1_tmax_full = lens.get_LENS_jan_tmax_QNW()
+
+lens1_gmst = lens1_gmst_full.sel(time=slice('1920', '2021'))
+lens1_tmax = lens1_tmax_full.sel(time=slice('1920', '2021'))
+
+lens1_gmst_arr = np.tile(lens1_gmst.values, lens1_tmax.shape[0])
+lens1_tmax_arr = np.ravel(lens1_tmax.values)
+
+# # bootstrap
+nboot = 1000
+bspreds_mu0 = np.zeros((nboot,))
+bspreds_sigma0 = np.zeros((nboot,))
+bspreds_alpha = np.zeros((nboot,))
+
+for i in range(nboot):
+    qn_i, sm_i = bootstrap(lens1_tmax_arr, lens1_gmst_arr)
+    xopt_i = pmath.mle_norm_2d_fast(qn_i, sm_i, [29.55, 1.03, 1.11])
+    bspreds_mu0[i] = xopt_i[0]
+    bspreds_sigma0[i] = xopt_i[1]
+    bspreds_alpha[i] = xopt_i[2]
+
+iter = np.arange(nboot)
+ds = xr.Dataset({
+    'mu0':    xr.DataArray(bspreds_mu0,    coords=[iter], dims=['iter']),
+    'sigma0': xr.DataArray(bspreds_sigma0, coords=[iter], dims=['iter']),
+    'alpha':  xr.DataArray(bspreds_alpha,  coords=[iter], dims=['iter']), 
+})
+filepath = '../../../megafires_data/output/MLE_tasmax_jan_LENS1_GMST_'+str(nboot)+'_normal_evaluation.nc'
+ds.to_netcdf(filepath)
--- processing/processing/lens.py
+++ processing/processing/lens.py
@@ -2,6 +2,7 @@ import xarray as xr
 import numpy as np
 
 def get_LENS_jan_tmax_QNW():
+    # nearest neighbor
     basedir = '../../../megafires_data/LENS/'
     filename = 'LENS_tmax_mon_QN.nc'
     filepath = basedir + filename
@@ -22,7 +23,17 @@ def get_LENS_jan_tmax_QNWE():
     qne = get_LENS_jan_tmax_QNE().drop(['lat','lon'])
     return (qnw + qne)/2.0
 
+def get_LENS_jan_tmax_QNW_control_run():
+    # nearerst neighbor
+    basedir = '../../../megafires_data/LENS/'
+    filename = 'LENS_tasmax_mon_mean_QN_control_run.nc'
+    filepath = basedir + filename
+    ds = xr.open_dataset(filepath)
+    da = ds['TREFMXAV']
+    return da
+
 def get_LENS_jan_tmax_QNWE_control_run():
+    # FIX: this is the version for NN
     basedir = '../../../megafires_data/LENS/'
     filename = 'LENS_tasmax_mon_mean_QN_control_run.nc'
     filepath = basedir + filename
@@ -31,6 +42,7 @@ def get_LENS_jan_tmax_QNWE_control_run():
     return da
 
 def get_LENS2_jan_tmax_QNW():
+    # nearest neighbor
     basedir = '../../../megafires_data/LENS2/'
     filename = 'LENS2_tmax_mean_mon_QNW.nc'
     filepath = basedir + filename
